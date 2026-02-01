bd651106245214ebc011fc693bb60e8b9f108de9
Kanif <kanifbansode0@gmail.com>
2026-02-01 16:09:49 +0530

    blogs and search feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,14 +5,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from blogs.models import Category, Blog
+from assignments.models import About
 
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
-    print(posts)
+    
+    # fetch About us
+    try:
+        about = About.objects.get()
+    except:
+        about = None
+
     context = {
         'featured_posts': featured_posts,
         'posts': posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
--- blogs/urls.py
+++ blogs/urls.py
@@ -8,10 +8,3 @@ urlpatterns = [
     path('<int:category_id>/', views.posts_by_category, name='posts_by_category')
 ]   
 
-# urlpatterns = [
-#     path('category/<int:category_id>/', views.posts_by_category, name='posts_by_category'),
-# ]
-
-# urlpatterns = [
-#     path('<int:category_id>/', views.posts_by_category, name='posts_by_category'),
-# ]
