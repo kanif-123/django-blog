edf01f445d603e99417ff8682edd1e70041ac178
Kanif <kanifbansode0@gmail.com>
2026-02-15 16:10:30 +0530

    Blog Post Crud Operations/features

--- blog_main/forms.py
+++ blog_main/forms.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import AuthenticationForm
 
 class RegistrationForm(UserCreationForm):  
     class Meta:
         model = User
         fields = ('email', 'username', 'password1', 'password2')
+
+
+# class LoginForm(AuthenticationForm):
+#     class Meta:
+#         model 
