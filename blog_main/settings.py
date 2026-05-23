3df1f1aeddb4b2bcf376accb3eee3c39a752a578
Kanif <kanifbansode0@gmail.com>
2026-05-23 13:00:31 +0530

    Updated ALLOWED_HOSTS with correct domain

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f7e4j*xct#yepb-o()5ce#6vo+xv&=g%wk@rb!(@x_j0-o28-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kanifb.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
